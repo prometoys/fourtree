bd7561f38b49f29805a39735ba363a59439d025f
Keywan <me@prometoys.net>
2010-10-14 17:55:53 +0200

    dbupdate code von helmut schottmueller

--- SyntaxTree/sql/dbupdate.php
+++ SyntaxTree/sql/dbupdate.php
@@ -1,36 +1,156 @@
 <#1>
-DROP TABLE IF EXISTS `il_qpl_qst_syntaxtree_question`;
-CREATE TABLE IF NOT EXISTS `il_qpl_qst_syntaxtree_question` (
-  `question_fi` int(11) NOT NULL default '0',
-  `textgap_rating` enum('ci','cs','l1','l2','l3','l4','l5') default NULL,
-  `correctanswers` int(11) default '0',
-  PRIMARY KEY  (`question_fi`)
-) ENGINE=MyISAM;
+<?
+if($ilDB->tableExists('il_qpl_qst_syntaxtree_question'))
+        $ilDB->dropTable('il_qpl_qst_syntaxtree_question');
 
-DROP TABLE IF EXISTS `il_qpl_qst_syntaxtree_answer`;
-CREATE TABLE IF NOT EXISTS `il_qpl_qst_syntaxtree_answer` (
-  `answer_id` int(10) unsigned NOT NULL auto_increment,
-  `question_fi` int(10) unsigned NOT NULL default '0',
-  `answertext` text NOT NULL,
-  `points` double NOT NULL default '0',
-  `aorder` int(10) unsigned NOT NULL default '0',
-  `lastchange` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`answer_id`),
-  KEY `question_fi` (`question_fi`)
-) ENGINE=MyISAM;
+if(!$ilDB->tableExists('il_qpl_qst_syntaxtree_question'))
+{
+        $fields = array (
+                'question_fi' => array
+                (
+                        'type' => 'integer',
+                        'length'  => 4,
+                        'notnull' => true,
+                        'default' => 0
+                ),
+                'textgap_rating'   => array
+                (
+                        'type' => 'text',
+                        'notnull' => false,
+                        'length' => 2,
+                        'default' => null
+                ),
+                'correctanswers' => array
+                (
+                        'type' => 'integer',
+                        'length'  => 4,
+                        'notnull' => true,
+                        'default' => 0
+                )
+        );
+        $ilDB->createTable('il_qpl_qst_syntaxtree_question', $fields);
+        $ilDB->addIndex('il_qpl_qst_syntaxtree_question', array('question_fi'), 'i1');
+}
 
+if($ilDB->tableExists('il_qpl_qst_syntaxtree_answer'))
+        $ilDB->dropTable('il_qpl_qst_syntaxtree_answer');
 
+if(!$ilDB->tableExists('il_qpl_qst_syntaxtree_answer'))
+{
+        $fields = array (
+                'answer_id' => array
+                (
+                        'type' => 'integer',
+                        'length'  => 4,
+                        'notnull' => true,
+                        'default' => 0
+                ),
+                'question_fi' => array
+                (
+                        'type' => 'integer',
+                        'length'  => 4,
+                        'notnull' => true,
+                        'default' => 0
+                ),
+                'answertext'   => array
+                (
+                        'type' => 'text',
+                        'notnull' => false,
+                        'length' => 4000,
+                        'default' => null
+                ),
+                'points' => array
+                (
+                        'type' => 'float',
+                        'notnull' => true,
+                        'default' => 0
+                ),
+                'aorder' => array
+                (
+                        'type' => 'integer',
+                        'length'  => 4,
+                        'notnull' => true,
+                        'default' => 0
+                ),
+                'lastchange' => array
+                (
+                        'type' => 'integer',
+                        'length'  => 4,
+                        'notnull' => true,
+                        'default' => 0 
+                ), 
+        );
+        $ilDB->createTable('il_qpl_qst_syntaxtree_answer', $fields);
+        $ilDB->addPrimaryKey('il_qpl_qst_syntaxtree_answer',array('answer_id'));
+        $ilDB->addIndex('il_qpl_qst_syntaxtree_answer', array('question_fi'), 'i1');
+        $ilDB->createSequence('il_qpl_qst_syntaxtree_answer');
+}
+?>
 <#2>
-REPLACE INTO `qpl_qst_type`(type_tag, plugin) VALUES ('SyntaxTree', 1);
+<?php
+$res = $ilDB->queryF("SELECT * FROM qpl_qst_type WHERE type_tag = %s",
+        array('text'),
+        array('SyntaxTree')
+);
+if ($res->numRows() == 0)
+{
+        $res = $ilDB->query("SELECT MAX(question_type_id) maxid FROM qpl_qst_type");
+        $data = $ilDB->fetchAssoc($res);
+        $max = $data["maxid"] + 1;
 
+        $affectedRows = $ilDB->manipulateF("INSERT INTO qpl_qst_type (question_type_id, type_tag, plugin) VALUES (%s, %s, %s)", 
+                array("integer", "text", "integer"),
+                array($max, 'SyntaxTree', 1)
+        );
+}
+?>
 <#3>
-DROP TABLE IF EXISTS `il_qpl_qst_syntaxtree_feedback`;
-CREATE TABLE IF NOT EXISTS `il_qpl_qst_syntaxtree_feedback` (
-  `feedback_id` int(11) unsigned NOT NULL default '0',
-  `question_fi` int(11) unsigned NOT NULL default '0',
-  `answer` int(11) unsigned NOT NULL default '0',
-  `feedback` text NOT NULL, 
-  `tstamp` int(11) unsigned NOT NULL default '0',
-  PRIMARY KEY (`feedback_id`),
-  KEY `question_fi` (`question_fi`)
-) ENGINE=MyISAM;
+<?php
+if($ilDB->tableExists('il_qpl_qst_syntaxtree_feedback'))
+        $ilDB->dropTable('il_qpl_qst_syntaxtree_feedback');
+
+if(!$ilDB->tableExists('il_qpl_qst_syntaxtree_feedback'))
+{
+        $fields = array (
+                'feedback_id' => array
+                (
+                        'type' => 'integer',
+                        'length'  => 4,
+                        'notnull' => true,
+                        'default' => 0
+                ),
+                'question_fi' => array
+                (
+                        'type' => 'integer',
+                        'length'  => 4,
+                        'notnull' => true,
+                        'default' => 0
+                ),
+                'answer' => array
+                (
+                        'type' => 'integer',
+                        'length'  => 4,
+                        'notnull' => true,
+                        'default' => 0
+                ),
+                'feedback'   => array
+                (
+                        'type' => 'text',
+                        'notnull' => false,
+                        'length' => 4000,
+                        'default' => null
+                ),
+                'tstamp' => array
+                (
+                        'type' => 'integer',
+                        'length'  => 4,
+                        'notnull' => true,
+                        'default' => 0
+                ),
+        );
+        $ilDB->createTable('il_qpl_qst_syntaxtree_feedback', $fields);
+        $ilDB->addPrimaryKey('il_qpl_qst_syntaxtree_feedback',array('feedback_id'));
+        $ilDB->addIndex('il_qpl_qst_syntaxtree_feedback', array('question_fi'), 'i1');
+        $ilDB->createSequence('il_qpl_qst_syntaxtree_feedback');
+}
+?>
